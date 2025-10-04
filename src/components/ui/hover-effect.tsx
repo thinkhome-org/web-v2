5d194f13d9c49d9312f56d68d1b5bc893fccb062
Erdinc Daskin <eruanio33@gmail.com>
2025-10-04 19:50:00 +0200

    Fixed, forgot curly brace

--- src/components/ui/hover-effect.tsx
+++ src/components/ui/hover-effect.tsx
@@ -18,14 +18,14 @@ export interface HoverEffectProps {
 export function HoverEffect({ items, className }: HoverEffectProps) {
   const [hoveredIndex, setHoveredIndex] = useState<number | null>(null);
 
- return (
-   <div className={cn("grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 py-10", className)}>
+  return (
+    <div className={cn("grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 py-10", className)}>
       {items.map((item, idx) => (
         <Link
           href={item?.link}
           key={item?.link}
           className="relative group block p-2 h-full w-full"
-         /* ten efekt je epileptickej a stejne nefunguje, + nehodi se ke zbytku
+          /* ten efekt je epileptickej a stejne nefunguje, + nehodi se ke zbytku
           onMouseEnter={() => setHoveredIndex(idx)}
           onMouseLeave={() => setHoveredIndex(null)}
           */
@@ -102,3 +102,4 @@ export function CardDescription({
       {children}
     </p>
   );
+}
